33a6ab28dcb4491b6c12e6a7edd2529c49d627e6
Leopold <patapon297@aim.com>
2019-02-18 12:31:28 +0100

    startup colorwipe and reduced LED brightness

--- led.py
+++ led.py
@@ -9,7 +9,7 @@ LED_PIN = 12  # GPIO pin connected to the pixels (18 uses PWM!).
 LED_PIN = 12  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 128  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP = ws.WS2811_STRIP_GRB
@@ -247,8 +247,9 @@ def light():
                               LED_STRIP)
     # Intialize the library (must be called once before other functions).
     strip.begin()
-    bpm = 155
+    bpm = 180
     lasttask = "Off"
+    colorWipe(strip, Color(255,255,255))
 
     while getattr(t, "do_run", True):
         task = getattr(t, "task")
